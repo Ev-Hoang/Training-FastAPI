f2d9553b37c6a8697804763f7e290ef7c036415d
Ev-Hoang <hduckien@gmail.com>
2025-07-08 11:51:37 +0700

    Update manage.py
    Add Root() to manage.py ( so that test.py ) actually work

--- manage.py
+++ manage.py
@@ -8,6 +8,10 @@ api = FastAPI()
 api.middleware("http")(log_requests)
 api.include_router(task_router)
 
+@api.get("/")
+def root():
+    return {"message": "Hello, world"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("manage:api", host="0.0.0.0", port=8000, reload=True)
